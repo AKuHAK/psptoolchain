3a9e062094cce7f6326dfc6442408154bfa935a3
Francisco Javier Trujillo Mata <fjtrujy@gmail.com>
2021-05-24 15:40:03 +0200

    Improve newlib

--- scripts/004-newlib.sh
+++ scripts/004-newlib.sh
@@ -4,7 +4,7 @@
 ## Download the source code.
 REPO_URL="https://github.com/fjtrujy/newlib.git"
 REPO_FOLDER="newlib"
-BRANCH_NAME="allegrex-v4.1.0"
+BRANCH_NAME="allegrex-v4.1.0-posix"
 if test ! -d "$REPO_FOLDER"; then
 	git clone --depth 1 -b $BRANCH_NAME $REPO_URL $REPO_FOLDER && cd $REPO_FOLDER || exit 1
 else
@@ -20,7 +20,10 @@ PROC_NR=$(getconf _NPROCESSORS_ONLN)
 rm -rf build-$TARGET && mkdir build-$TARGET && cd build-$TARGET || { exit 1; }
 
 ## Configure the build.
-../configure --prefix="$PSPDEV" --target="$TARGET" || { exit 1; }
+../configure \
+	--prefix="$PSPDEV" \
+	--target="$TARGET" \
+	$TARG_XTRA_OPTS || { exit 1; }
 
 ## Compile and install.
 make --quiet -j $PROC_NR clean          || { exit 1; }
