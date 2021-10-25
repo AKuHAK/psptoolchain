61e70cea7a2cd515ddce38c9b79c69e50cd46361
Francisco Javier Trujillo Mata <fjtrujy@gmail.com>
2021-10-25 23:40:14 +0200

    Improve dir POSIX functions

--- scripts/004-newlib.sh
+++ scripts/004-newlib.sh
@@ -4,7 +4,7 @@
 ## Download the source code.
 REPO_URL="https://github.com/fjtrujy/newlib.git"
 REPO_FOLDER="newlib"
-BRANCH_NAME="allegrex-v4.1.0-lock"
+BRANCH_NAME="allegrex-v4.1.0-dir"
 if test ! -d "$REPO_FOLDER"; then
 	git clone --depth 1 -b $BRANCH_NAME $REPO_URL && cd $REPO_FOLDER || { exit 1; }
 else
